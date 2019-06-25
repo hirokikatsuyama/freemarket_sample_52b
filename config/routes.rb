46fbe1e5b485d365e694c7ab164e7654d1a67da5
米村弘海 <hiroumiyonemura@yonemurahirominoMacBook-Pro.local>
2019-06-25 09:22:57 +0900

    revise routing to see easily

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,32 @@
 Rails.application.routes.draw do
   root to: "items#index"
-  get 'users/sign_up' => 'users#sign_up'
-  get 'users/registration_base' => 'users#registration_base'
-  get 'users/registration_phone' => 'users#registration_phone'
-  get 'users/registration_address' => 'users#registration_address'
-  get 'users/registration_payment' => 'users#registration_payment'
-  get 'users/registration_completion' => 'users#registration_completion'
-  get 'users/confirmation' => 'users#confirmation'
   post 'users/registration_base' => 'users#create'
   get 'sessions/sign_in' => 'sessions#sign_in'
   post 'sessions/create' =>'sessions#create'
   delete 'sessions/destroy' => 'sessions#destroy'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/logout'
   # resources :users, only: [:sign_up, :registration_base, :registration_phone, :registration_address, :registration_payment, :registration_completion, :confirmation, :registration_base, :edit, :show, :logout]
   # resources :sessions, only: [:sign_in, :create, :destroy]
   resources :credits, only: [:index, :new]
   resources :purchase, only: [:new]
+
+  resources :users, only: [:edit, :show] do
+    collection do
+      delete 'log_out'
+      get 'log_out_page'
+      get 'sign_up'
+      get 'registration_base'
+      get 'registration_phone'
+      get 'registration_address'
+      get 'registration_payment'
+      get 'registration_completion'
+      get 'confirmation'
+    end
+  end
+
   resources :items, only: [:index, :new, :create, :show] do
     collection do
       get 'search'
     end
   end
   resources :categories, only: [:index, :show]
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-
 end
