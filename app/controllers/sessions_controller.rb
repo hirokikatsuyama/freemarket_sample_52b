74ec019d6074a56d1f6bf7984b3ff1d3cfede77a
米村弘海 <hiroumiyonemura@yonemurahirominoMacBook-Pro.local>
2019-06-25 19:54:29 +0900

    delete sessions sign-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,4 @@
 class SessionsController < ApplicationController
-  def sign_in
-  end
-
-  def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to root_path
-    else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'sign_in'
-    end
-  end
 
   def destroy
     log_out
