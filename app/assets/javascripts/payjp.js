d8d7913f0277fa440bd9e637451dd805910103c4
hirokikatsuyama <50540371+hirokikatsuyama@users.noreply.github.com>
2019-06-26 18:36:56 +0900

    エラー解決のため

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -2,7 +2,7 @@ $(function(){
 
 var form = $("#charge-form");
   Payjp.setPublicKey("pk_test_a8d83b01f25e72078fc1e347");
-  $("#charge-form-button").on("click",function(e){
+  $("#charge-form").on("submit",function(e){
     e.preventDefault();
     var card = {
       number: $("#card_number").val(),
@@ -22,11 +22,13 @@ var form = $("#charge-form");
         $("#cvc").removeAttr("name");
         $("#exp_month").removeAttr("name");
         $("#exp_year").removeAttr("name");
+        alert("クレジットカードの登録が完了しました");
+
         var token = response.id;
         $("#charge-form").append(`<input type="hidden" name="pay_id" class="payjp-token" value=${token} />`)
         $("#charge-form").get(0).submit();
       }else{
-        alert("カード情報が正しくありません。");
+        alert("カード情報が正しくありません");
       }
     });
   });
