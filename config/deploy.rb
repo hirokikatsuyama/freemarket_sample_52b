605a38d520e3fbfc5ba47bd13e6202dbd9ed06a9
米村弘海 <hiroumiyonemura@yonemurahirominoMacBook-Pro.local>
2019-06-26 12:19:04 +0900

    revise link file of master.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "freemarket_sample_52b"
 set :repo_url, 'git@github.com:hiro1301560/freemarket_sample_52b.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+set :linked_files, fetch(:linked_files, []).push("shared/config/master.key")
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
