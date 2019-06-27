b8ca31a3856642a96f22c1152e1a701882b43519
hirokikatsuyama <50540371+hirokikatsuyama@users.noreply.github.com>
2019-06-27 20:43:00 +0900

    creditsのmigration file統合

--- db/migrate/20190619074049_add_column_to_credits.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToCredits < ActiveRecord::Migration[5.2]
-  def change
-    add_column :credits, :customer_id, :integer
-    add_column :credits, :card_id, :integer
-  end
-end
--- db/migrate/20190625065144_add_columns.rb
+++
@@ -1,7 +0,0 @@
-class AddColumns < ActiveRecord::Migration[5.2]
-  def change
-    add_column :credits, :customer_id, :string
-    add_column :credits, :card_id, :string
-    t.references :user, foreign_key:true
-  end
-end
