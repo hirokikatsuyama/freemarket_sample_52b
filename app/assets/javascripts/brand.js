9f94ee0b5acfba39159381604b11d68b0127a62c
米村弘海 <hiroumiyonemura@yonemurahirominoMacBook-Pro.local>
2019-06-29 08:03:48 +0900

    delete possible error factor for deploying

--- app/assets/javascripts/brand.js
+++ app/assets/javascripts/brand.js
@@ -12,7 +12,7 @@
 
 //   function appendNewUser(id, name){
 //     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id} data-id=${id}'>
-//                   <input name='group[user_ids][]' type='hidden' value='${id}'>
+//                   <input name='group[user_ids]' type='hidden' value='${id}'>
 //                   <p class='chat-group-user__name'>${name}</p>
 //                   <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
 //                 </div>`
