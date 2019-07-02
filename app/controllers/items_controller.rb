6bc30f807994b70226a0094181ec2b942da645d3
米村弘海 <hiroumiyonemura@yonemurahirominoMacBook-Pro.local>
2019-07-02 16:57:26 +0900

    revise item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,15 @@
 class ItemsController < ApplicationController
-
-  before_action :set_item, only: [:show, :edit, :destroy,]
-  before_action :set_transaction, only: [:create]
+  before_action :set_item, only: [:show, :edit, :destroy]
 
   def index
-    @lady_items = Item.includes(:images).where(category_id: Category.find(1).subtree_ids).order(created_at: "DESC").limit(4)
-    @man_items = Item.includes(:images).where(category_id: Category.find(2).subtree_ids).order(created_at: "DESC").limit(4)
-    @kids_items = Item.includes(:images).where(category_id: Category.find(3).subtree_ids).order(created_at: "DESC").limit(4)
-    @beauty_items = Item.includes(:images).where(category_id: Category.find(7).subtree_ids).order(created_at: "DESC").limit(4)
-    @chanel = Item.includes(:images).where(brand_id: 1).order(created_at: "DESC").limit(4)
-    @vuitton = Item.includes(:images).where(brand_id: 2).order(created_at: "DESC").limit(4)
-    @supreme = Item.includes(:images).where(brand_id: 3).order(created_at: "DESC").limit(4)
-    @nike = Item.includes(:images).where(brand_id: 4).order(created_at: "DESC").limit(4)
+    @lady_items = Item.includes(:images).where(category_id: Category.find(1).subtree_ids).where(status: 1).order(created_at: "DESC").limit(4)
+    @man_items = Item.includes(:images).where(category_id: Category.find(2).subtree_ids).where(status: 1).order(created_at: "DESC").limit(4)
+    @kids_items = Item.includes(:images).where(category_id: Category.find(3).subtree_ids).where(status: 1).order(created_at: "DESC").limit(4)
+    @beauty_items = Item.includes(:images).where(category_id: Category.find(7).subtree_ids).where(status: 1).order(created_at: "DESC").limit(4)
+    @chanel = Item.includes(:images).where(brand_id: 1).where(status: 1).order(created_at: "DESC").limit(4)
+    @vuitton = Item.includes(:images).where(brand_id: 2).where(status: 1).order(created_at: "DESC").limit(4)
+    @supreme = Item.includes(:images).where(brand_id: 3).where(status: 1).order(created_at: "DESC").limit(4)
+    @nike = Item.includes(:images).where(brand_id: 4).where(status: 1).order(created_at: "DESC").limit(4)
   end
 
   def new
@@ -42,10 +40,9 @@ class ItemsController < ApplicationController
       end
     end
     @item = Item.create!(item_params)
-    @transaction.seller_id = @item.user_id
-    @transaction.item_id = @item.id
-    @transaction.status = 1
-    @transaction.save
+    @item.status = 1
+    @item.save
+    Transaction.create(seller_id: @item.user_id, item_id: @item.id, status: 1)
     redirect_to controller: :items, action: :index
   end
 
@@ -85,20 +82,8 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    # if item.user_id == current_user_id
     item.destroy
     redirect_to root_path
-    # else
-    #   redirect_to root_path alert: "エラーが発生しました。"
-    # end
-  private
-    def item_params
-      params.require(:item).permit(:name, :detail, :condition, :shipping_cost, :delivery_date, :shipping_source, :price,{images: []}, :brand_id, :size_id).merge(user_id: current_user.id)
-    end
-
-    def set_transaction
-      @transaction = Transaction.new
-    end
   end
 
   def keyword_search
