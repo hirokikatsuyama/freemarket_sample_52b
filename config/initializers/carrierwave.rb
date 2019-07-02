ea26e904cf8a4900f70e98f69713965a36d3e733
mio-158cm <hayakawan18@gmail.com>
2019-07-02 16:47:36 +0900

    change-setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,8 +14,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
     }
+    config.fog_directory  = 'freemarket-image-upload'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-image-upload'
   end
-
-  config.fog_directory  = 'freemarket-image-upload'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-image-upload'
 end
