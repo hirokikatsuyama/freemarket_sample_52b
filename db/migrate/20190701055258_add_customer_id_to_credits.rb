b28da1e73bbe6e5cf0f041574864584cc1031a37
米村弘海 <hiroumiyonemura@yonemurahirominoMacBook-Pro.local>
2019-07-04 14:32:16 +0900

    remove unnecessary column

--- db/migrate/20190701055258_add_customer_id_to_credits.rb
+++
@@ -1,7 +0,0 @@
-class AddCustomerIdToCredits < ActiveRecord::Migration[5.2]
-  def change
-    add_column :credits, :customer_id, :string
-    add_column :credits, :card_id, :string
-
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_01_055258) do
+ActiveRecord::Schema.define(version: 2019_07_01_054229) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -82,8 +82,6 @@ ActiveRecord::Schema.define(version: 2019_07_01_055258) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.string "customer_id"
-    t.string "card_id"
     t.index ["user_id"], name: "index_credits_on_user_id"
   end
 
