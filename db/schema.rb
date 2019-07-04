b9d181b361d8fb7c5d7e0e39706a35370d9a0121
米村弘海 <hiroumiyonemura@yonemurahirominoMacBook-Pro.local>
2019-07-04 15:00:23 +0900

    add two columns to credit table

--- db/schema.rb
+++ db/schema.rb
@@ -79,6 +79,8 @@ ActiveRecord::Schema.define(version: 2019_07_01_054229) do
   end
 
   create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "customer_id"
+    t.string "card_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
