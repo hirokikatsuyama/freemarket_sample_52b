63f47702273b5c0b9b277543dce47dda26322b3e
米村弘海 <hiroumiyonemura@yonemurahirominoMacBook-Pro.local>
2019-07-08 14:41:23 +0900

    add asset host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'freemarket-image-upload'
-    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-image-upload'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-image-upload'
   end
 
 end
